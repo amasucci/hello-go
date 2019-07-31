76bb055fddfd3982faf882c68248ea4a5ceddaf9
Antonio Masucci <antmas83@gmail.com>
2019-07-31 15:29:11 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,13 +6,6 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", helloGo)
-	http.HandleFunc("/healthz", healthCheckHandler)
-	log.Print("Listening on port 9090")
-	log.Fatal(http.ListenAndServe(":9090", nil))
-}
-
 func helloGo(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
@@ -23,4 +16,11 @@ func helloGo(w http.ResponseWriter, r *http.Request) {
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "All Good")
+}
+
+func main() {
+	http.HandleFunc("/", helloGo)
+	http.HandleFunc("/healthz", healthCheckHandler)
+	log.Print("Listening on port 9090")
+	log.Fatal(http.ListenAndServe(":9090", nil))
 }

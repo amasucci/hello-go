410e0d491ab7669afe18c7d329c47b70a6ccac28
Antonio Masucci <antmas83@gmail.com>
2019-07-31 14:33:48 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", helloGo)
+	http.HandleFunc("/healthz", healthCheckHandler)
+	log.Print("Listening on port 9090")
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
+
+func helloGo(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello Go!")
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "All Good")
+}
